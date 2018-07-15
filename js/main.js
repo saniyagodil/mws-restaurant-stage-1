710d4fcc9c93c1e962829a5d9a8baa30cf1f6e3a
saniyagodil <saniyagodil@gmail.com>
2018-07-15 17:22:22 -0400

    Attempt to add alt text to images'

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = 'Picture from ' + restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
