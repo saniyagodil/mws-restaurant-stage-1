8d11e3950d405725249e706706ea24a3ca31a6f4
saniyagodil <saniyagodil@gmail.com>
2018-08-18 18:52:13 -0400

    Switch to server for restaurant information and files

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,8 +9,9 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `http://localhost:1337/restaurants`;
   }
+  //change url 
 
   /**
    * Fetch all restaurants.
