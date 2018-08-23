1938585cd01a4c0375f70038f195fa17e8790274
saniyagodil <saniyagodil@gmail.com>
2018-08-22 20:58:21 -0400

    Service worker only if cached/same origin

--- sw.js
+++ sw.js
@@ -28,10 +28,12 @@ self.addEventListener('install', function(event){
 
 
 self.addEventListener('fetch', function(event){
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      if(response) return response;
-      return fetch(event.request);
-    })
-  );
+  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin')
+    return
+      event.respondWith(
+        caches.match(event.request).then(function(response){
+          if(response) return response;
+          return fetch(event.request);
+        })
+      );
 });
